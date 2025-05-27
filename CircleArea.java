8d24e267af759a71ec669b3e0b37f5cf635fc295
Misba-1323 <misbakousarrjamadar@gmail.com>
2025-05-27 12:06:51 +0530

    save

---
+++ CircleArea.java
@@ -0,0 +1,14 @@
+//Circleapackage CircleRectangle;
+
+public class CircleArea {
+    public static void main(String[] args) {
+        double radius = 5;
+
+        double area = 3.14 * radius * radius;
+        double perimeter = 2 * 3.14 * radius;
+
+        System.out.println("Area of circle = "+area);
+        System.out.println("Perimeter of circle = "+perimeter);
+        
+    }
+}
