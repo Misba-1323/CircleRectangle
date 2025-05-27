a84974c0064c1ef5046267b0b43e76f8199224d2
Misba-1323 <misbakousarrjamadar@gmail.com>
2025-05-27 12:08:38 +0530

    saved

---
+++ RectanglePerimeter.java
@@ -0,0 +1,14 @@
+//package CircleRectangle;
+
+public class RectanglePerimeter {
+    public static void main(String[] args) {
+        double length = 10;
+        double breadth = 20;
+
+        double area = length*breadth;
+        double perimeter = 2*(length+breadth);
+
+        System.out.println("Area of rectangle = "+area);
+        System.out.println("Perimeter of rectangle = "+perimeter);
+    }
+}
